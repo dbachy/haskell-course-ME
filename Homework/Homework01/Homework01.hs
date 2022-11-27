f17879a18f56de03574af1f94a83060dc6c8076a
dbachy <computer321@dbach.org>
2022-11-27 15:47:44 +0000

    incremental commit, DBR

--- Homework/Homework01/Homework01.hs
+++ Homework/Homework01/Homework01.hs
@@ -9,6 +9,8 @@ DBR 11/26/2022 @ 11:02PM-}
 -- Question 2
 -- Define a function that takes a value and multiplies it by 3.
 
+fun x = x*3
+
 -- Question 3
 -- Define a function that calculates the area of a circle.
 
