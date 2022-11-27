cbeae8053cb7cf5989983e11618599c195574333
dbachy <computer321@dbach.org>
2022-11-27 16:38:17 +0000

    DBR commit

--- Homework/Homework01/Homework01.hs
+++ Homework/Homework01/Homework01.hs
@@ -14,10 +14,14 @@ fun x = x*3
 -- Question 3
 -- Define a function that calculates the area of a circle.
 
+areaOfACircle r = pi * r^2
 
 -- Question 4
 -- Define a function that calculates the volume of a cylinder by composing the previous function together with the height of the cylinder. 
 
+volumeOfACylinder r h = areaOfACircle r * h
+
 -- Question 5
 -- Define a function that takes the height and radius of a cylinder and checks if the volume is greater than or equal to 42.
 
+checkVolume42 v = v == 42
