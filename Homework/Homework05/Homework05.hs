e0169c8ebd514854a192bfd72d1b45f941ff5a9e
dbachy <computer321@dbach.org>
2022-12-18 15:37:32 +0000

    Starting homework to ch 5

--- Homework/Homework05/Homework05.hs
+++ Homework/Homework05/Homework05.hs
@@ -2,14 +2,19 @@
 -- flips the order of the parameters.
 -- For example this: `(/) 6 2` returns `3`. But this: `flip' (/) 6 2` returns `0.3333333333`
 
+thingRev f a b  = flip f a b
 
 -- Create the `uncurry'` function that converts a curried function to a function on pairs. So this: `(+) 1 2` that returns `3` can be written as
 -- `uncurry' (+) (1,2)` (with the two different arguments inside a pair).
 
+uncurry' :: (a -> b -> c) -> (a,b) -> c
+uncurry' f (x,y) = f x y
 
 -- Create the `curry'` function that converts an uncurried function to a curried function. So this: `fst (1,2)` that returns `1` can be written as
 -- `curry' fst 1 2` (with the tuple converted into two different arguments).
 
+curry' :: ((x,y) -> f) -> x -> y -> f
+curry' f x y = f (x,y)
 
 -- Use higher-order functions, partial application, and point-free style to create a function that checks if a word has an uppercase letter.
 -- Start with using just higher-order functions and build from there. 
